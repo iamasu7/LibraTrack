1acfdc080d2d0a28912836e730dc9bd33b5e0f44
iamasu7 <sanyangassan107@gmail.com>
2026-09-15 17:07:38 +0000

    feat: resized arrays - add items to the arrays using user keyboard

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -6,6 +6,50 @@ public class Main {
         String[] location = {"Brusubi","Serrekunda","Lamin","Farato","Bakau"};
         boolean[] paidFees = {true,true,false,true,false};
 
-        
+        Scanner in = new Scanner(System.in);
+
+        do {
+            System.out.println("Are You Adding A New Student? Y or N");
+            String userInput = in.next();
+
+            if (userInput.equalsIgnoreCase("n"))break;
+            System.out.println("Enter Student Name: ");
+            String name = in.next();
+
+            System.out.println("Enter Student Age: ");
+            int age = in.nextInt();
+
+            System.out.println("Enter Student Number: ");
+            int stuNum = in.nextInt();
+
+            System.out.println("Enter Student Location: ");
+            String lo = in.next();
+
+            System.out.println("Has The Student Paid The Tuition Fee: ");
+            boolean fees = in.nextBoolean();
+
+            studentName = Arrays.copyOf(studentName, studentName.length+1);
+            studentName[studentName.length-1] = name;
+            System.out.println(Arrays.toString(studentName));
+
+            studentAge = Arrays.copyOf(studentAge, studentAge.length+1);
+            studentAge[studentAge.length-1] = age;
+            System.out.println(Arrays.toString(studentAge));
+
+            studentNumber = Arrays.copyOf(studentNumber, studentNumber.length+1);
+            studentNumber[studentNumber.length-1] = stuNum;
+            System.out.println(Arrays.toString(studentNumber));
+
+            location = Arrays.copyOf(location, location.length+1);
+            location[location.length-1] = lo;
+            System.out.println(Arrays.toString(location));
+
+            paidFees = Arrays.copyOf(paidFees, paidFees.length+1);
+            paidFees[paidFees.length-1] = fees;
+            System.out.println(Arrays.toString(paidFees));
+
+        } while (true);
+
+       
     }
 }
