682b206229b34bd8e707ef099e7c70b4a5b18b95
iamasu7 <sanyangassan107@gmail.com>
2026-09-15 17:03:54 +0000

    feat: initialise student data arrays

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,2 +1,11 @@
 public class Main {
+    static void main(String[] args) {
+        String[] studentName = {"Assan", "Lamin", "John", "Peter","Andrew"};
+        int[] studentAge = {25,30,32,27,29};
+        int[] studentNumber = {1,2,3,4,5};
+        String[] location = {"Brusubi","Serrekunda","Lamin","Farato","Bakau"};
+        boolean[] paidFees = {true,true,false,true,false};
+
+        
+    }
 }
